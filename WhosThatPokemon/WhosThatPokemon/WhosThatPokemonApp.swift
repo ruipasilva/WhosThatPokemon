ba2b08e448dffd6fae80c2fdff362839a6cdc7b3
Rui Silva <ruipasilva@gmail.com>
2024-11-19 17:07:56 +0000

    Initial commit Added Xcode project and files

---
+++ WhosThatPokemon/WhosThatPokemon/WhosThatPokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhosThatPokemonApp.swift
+//  WhosThatPokemon
+//
+//  Created by Rui Silva on 19/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct WhosThatPokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
